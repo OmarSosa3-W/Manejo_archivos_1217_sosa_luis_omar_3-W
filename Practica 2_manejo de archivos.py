26f5e0a1b099314bcfbb26c90d16faea975147e4
OmarSosa3-W <a.23308051281217@cbtis128.edu.mx>
2024-10-25 19:37:48 -0600

    Add files via upload

---
+++ Practica 2_manejo de archivos.py
@@ -0,0 +1,7 @@
+print() #para una linea en blanco
+print("SOSA LUIS OMAR 1217: MI PRACTICA DE MANEJO DE ARCHIVOS") #nombre del credor
+print() #para una linea en blanco
+with open("Practica 2.txt", "r") as f:  #imprime la practica 2
+    contenido = f.read() 
+    print(contenido) #imprimira el contenido ingresado previamente
+print() #para una linea en blanco
